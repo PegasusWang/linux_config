ee433f0204a5eb290aa8f2116d3e41fd96b57b2a
wnn <wangningning@papayamobile.cn>
2016-05-04 17:58:14 +0800

    fix

--- vps_install.sh
+++ vps_install.sh
@@ -36,11 +36,35 @@ sudo pip install virtualenv
 #sys.setdefaultencoding('utf8')
 
 
-#modify capslock
-#sudo vi /etc/default/keyboard
-#修改下面这一行：（改变capslock为ctrl）
-#XKBOPTIONS="ctrl:nocaps"
-#或者可以交换capslock和ctrl
-#XKBOPTIONS="ctrl:nocaps"
-#最后：
-#sudo dpkg-reconfigure keyboard-configuration
+# pyenv need
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+    libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev
+
+
+# https://github.com/yyuu/pyenv
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+cp ./ubuntu_zshenv ~/.zshenv
+#exec $SHELL
+pyenv install 2.7.11
+pyenv install 3.5.0
+
+# https://github.com/yyuu/pyenv-virtualenv
+pyenv virtualenv 2.7.11 pyenv_2.7.11
+pyenv virtualenv 3.5.0 pyenv_3.5.0
+
+mkdir -p ~/project/pyenv_2.7.11
+mkdir -p ~/project/pyenv_3.5.0
+
+
+#for python
+#export EDITOR=vim
+#export PYTHONIOENCODING=UTF-8    # for python moidfy rc file bashrc or zshrc
+sudo apt-get install -y build-essential libssl-dev libevent-dev libjpeg-dev libxml2-dev libxslt-dev build-dep libpq-dev
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-distribute
+sudo apt-get install -y python-dev
+sudo apt-get install -y ipython
+sudo apt-get install -y zlib1g-dev
+sudo apt-get install -y libudev-dev
+sudo apt-get install -y libffi-dev   # for brycpt
+
