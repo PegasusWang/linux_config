94245d31b884b1c25243b524b4a906138f82723e
wnn <wangningning@papayamobile.cn>
2016-05-04 15:28:12 +0800

    add need lib for pyevn

--- py_ubuntu.sh
+++ py_ubuntu.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# pyenv need
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+    libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev
 # https://github.com/yyuu/pyenv
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 cp ./ubuntu_zshenv ~/.zshenv
@@ -18,7 +21,7 @@ pyenv install 3.5.0
 #for python
 #export EDITOR=vim
 #export PYTHONIOENCODING=UTF-8    # for python moidfy rc file bashrc or zshrc
-sudo apt-get install -y build-essential libssl-dev libevent-dev libjpeg-dev libxml2-dev libxslt-dev
+sudo apt-get install -y build-essential libssl-dev libevent-dev libjpeg-dev libxml2-dev libxslt-dev build-dep
 sudo apt-get install -y python-pip
 sudo apt-get install -y python-distribute
 sudo apt-get install -y python-dev
@@ -39,13 +42,6 @@ if [ -d "${PYENV_ROOT}" ]; then
   export PATH="${PYENV_ROOT}/bin:${PATH}"
   eval "$(pyenv init -)"
 fi
-# 安装必要包
-sudo apt-get build-dep python2.7
-pyenv install 2.7.10
-pyenv install 3.5.0
-
-#创建虚拟环境
-pyenv virtualenv 2.7.1 testenv271
 
 #for python utf-8
 #sudo vi /usr
