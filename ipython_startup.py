adb6089d82ccfe29cd6064e276cd5bd1360574e5
PegasusWang <291374108@qq.com>
2017-11-23 16:31:03 +0800

    add ipytohn startup.py

---
+++ ipython_startup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Ned's .startup.py file    ipython 启动加载文件，用来导入一些自定义函数或者模块
+# http://stackoverflow.com/questions/11124578/automatically-import-modules-when-entering-the-python-or-ipython-interpreter
+
+print("(.startup.py)")
+
+import datetime as dt
+import os
+import pprint
+import re
+import sys
+import time
+import json
+import requests as req
+
+try:
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    from pandas import Series, DataFrame
+    import numpy as np
+except ImportError:
+    pass
+
+print("(imported datetime, os, pprint, re, sys, time, json)")
+pp = pprint.pprint
