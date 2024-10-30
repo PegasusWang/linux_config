b0ed010b5a7a5aaab975f605adcfdc89b75d4ce3
PegasusWang <291374108@qq.com>
2024-10-30 11:46:01 +0800

    goland alias

--- mac/.env.sh
+++ mac/.env.sh
@@ -122,6 +122,7 @@ alias tnew='tmux new -s '
 alias tpl='tmuxp load '
 alias updatedb='sudo /usr/libexec/locate.updatedb'
 alias gmt='go mod tidy'
+alias goland='/Applications/GoLand.app/Contents/MacOS/goland '
 
 # youtube
 # python3 -m pip install --force-reinstall https://github.com/yt-dlp/yt-dlp/archive/master.tar.gz
