b046b40522379f927ffaf0c549a2a3fddee851f7
PegasusWang <291374108@qq.com>
2025-10-25 17:14:02 +0800

    uninstall python

---
+++ python/clean_python.sh
@@ -0,0 +1,18 @@
+# brew list | grep python
+#
+
+brew uninstall python@3.10
+brew unlink python@3.10
+brew cleanup -s python@3.10
+
+brew uninstall python@3.11
+brew unlink python@3.11
+brew cleanup -s python@3.11
+
+brew uninstall python@3.12
+brew unlink python@3.12
+brew cleanup -s python@3.12
+
+brew uninstall python@3.13
+brew unlink python@3.13
+brew cleanup -s python@3.13
